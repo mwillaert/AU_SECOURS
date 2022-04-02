16e5bea3c41d76cf7d56c47be939bd9abbaa43b6
aert12142 <101937396+aert12142@users.noreply.github.com>
2022-04-02 20:20:26 +0200

    a

--- src/model/Meteor.java
+++ src/model/Meteor.java
@@ -1,9 +1,37 @@
 package model;
+
+import java.util.List;
+
 public class Meteor extends Displayable{
     public Meteor(double _x, double _y, double _speedx, double _speedy) {
         super(_x, _y);
         this.width=30;
         this.height = 10;
         this.setSpeed(_speedx,_speedy);
+    }
+
+    @Override
+    public void setBoxCollision() {
+
+    }
+
+    @Override
+    public void setTypeObject() {
+
+    }
+
+    @Override
+    public void calculCollision(List<Displayable> objects) {
+
+    }
+
+    @Override
+    public int actionCollisionPerso(int arg, Player p) {
+        return 0;
+    }
+
+    @Override
+    public void gravity() {
+
     }
 }
